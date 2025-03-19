cfd21fc38b868aa327034179d8e08cd7cf7c6097
Dragos Daian <daian.dragos@yahoo.com>
2025-03-19 23:55:02 +0100

    just build macos arm64 for now

--- install-macos.sh
+++ install-macos.sh
@@ -9,9 +9,9 @@ cd ../..
 cd external/vcpkg
 ./bootstrap-vcpkg.sh
 ./vcpkg install boost-uuid --triplet arm64-osx
-./vcpkg install boost-uuid --triplet x64-osx
+#./vcpkg install boost-uuid --triplet x64-osx
 ./vcpkg install inih --triplet arm64-osx
-./vcpkg install inih --triplet x64-osx
+#./vcpkg install inih --triplet x64-osx
 ./vcpkg list
 ./vcpkg integrate install
 cd ../..
