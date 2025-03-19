fc18762f255af3827fa92d81dd3c68226550ae2e
Dragos Daian <daian.dragos@yahoo.com>
2025-03-19 23:02:06 +0100

    use powershell

---
+++ build_release.ps1
@@ -0,0 +1,18 @@
+# Remove the build directory if it exists
+Remove-Item -Recurse -Force -Path "build"
+
+# Create the build directory
+New-Item -ItemType Directory -Path "build"
+
+# Change to the build directory
+Set-Location -Path "build"
+
+# Run cmake with the specified arguments
+cmake -S .. -B . `
+    -G "Visual Studio 17 2022" `
+    -DCMAKE_TOOLCHAIN_FILE=external/vcpkg/scripts/buildsystems/vcpkg.cmake `
+    -DCMAKE_BUILD_TYPE=RelWithDebInfo `
+    -DCMAKE_CXX_FLAGS_RELWITHDEBINFO="/O2 /GL /DNDEBUG"
+
+# Go back to the previous directory
+Set-Location -Path ".."
---
+++ install-windows.ps1
@@ -0,0 +1,23 @@
+# Set WITH_ZLIB to 0
+$env:WITH_ZLIB = '0'
+
+# Build uWebSockets
+Set-Location -Path "external\uWebSockets"
+mingw32-make
+mingw32-make install
+Set-Location -Path "..\.."
+
+# Install packages using vcpkg
+Set-Location -Path "external\vcpkg"
+.\bootstrap-vcpkg.bat
+.\vcpkg install boost-uuid:x64-windows
+.\vcpkg install readerwriterqueue:x64-windows
+.\vcpkg install inih:x64-windows
+.\vcpkg integrate install
+Set-Location -Path "..\.."
+
+# Install luajit
+Set-Location -Path "external\luajit"
+mingw32-make
+mingw32-make install
+Set-Location -Path "..\.."
